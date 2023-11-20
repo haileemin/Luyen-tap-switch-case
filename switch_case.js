3d58659738cfd27ac56eaee74d54afc77389e732
haileemin <dr.leminhhai2003@gmail.com>
2023-11-20 17:10:14 +0700

    switch_case

---
+++ switch_case.js
@@ -0,0 +1,34 @@
+
+
+// // let b = 1;
+// // let c = 5;
+
+// // n = c + b;
+
+// // switch (n) {
+// //     case (3):
+// //         console.log("true");
+// //        break;
+// //     case (6):
+// //         console.log("false"); 
+// //        break;
+// //     default:
+// //         console.log("error");
+// // }
+
+
+// let day = 3;
+
+// switch (day) {
+//     case 4:
+//         console.log("Hôm nay là thứ 4");
+//         break;
+//      case 3:
+//         console.log("Hôm nay là thứ 3");
+//         // break;
+//     case 2:
+//         console.log("Hôm nay là thứ 2");
+//         break;
+//     default:
+//         console.log("Hôm nay là một ngày đẹp trời");
+// }
